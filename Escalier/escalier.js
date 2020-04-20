5934af00ca40c7e292cc25383dfa8947818d89d5
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-04-20 17:02:49 +0200

    Exercice escalier terminer

---
+++ Escalier/escalier.js
@@ -0,0 +1,13 @@
+var size = process.argv.slice(2);
+
+
+for(i=1; i<=size; i++){
+    for(y=size; y>i; y--){
+        process.stdout.write(' ');
+    }
+    
+    for(x=0; x<i; x++){
+        process.stdout.write('#');
+    }
+    console.log();
+}
