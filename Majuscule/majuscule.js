b823793eca58991d7c15265328f7b548bcb69889
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-04-21 12:39:00 +0200

    Ajout de lexercice majuscule

---
+++ Majuscule/majuscule.js
@@ -0,0 +1,25 @@
+var argument = process.argv.slice(2);
+
+argument = String(argument).toLowerCase();
+var count = 1;
+
+for(var i=0; i<argument.length; i++){
+    //console.log(argument[i].indexOf(' '));
+
+    if(argument[i].indexOf(' ') == 0){   
+    }
+    else if(count%2 == 0){
+        console.log(count);
+        argument = argument.replace(argument[i] ,argument[i].toUpperCase());
+        count = count + 1;  
+    }
+    else{
+        count = count + 1;
+    }
+}
+
+
+console.log(argument);
+
+
+
