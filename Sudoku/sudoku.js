44407f73fb0bb72bdbd65faba0aee1f0d3347b82
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-04-23 14:49:07 +0200

    Sudoku Solve

--- Sudoku/sudoku.js
+++ Sudoku/sudoku.js
@@ -1,32 +1,37 @@
 var arg = process.argv.slice(2);
 
+var sudoku = getSudokuGrid(arg[0]);
 
-getSudokuGrid(arg[0]);
+sudoku = resolveSudoku(sudoku);
+
+sudoku.forEach(element => console.log(element));
 
 
 
+
+
+
+
+
+
+//Get the data from the grid in a txt file 
 function getSudokuGrid(sudoku_path){
     let fs = require('fs');
     let sudoku = fs.readFileSync(sudoku_path, 'utf8');
 
-    sudokuInArray(sudoku);
+    sudoku = sudokuInArray(sudoku);
+    return sudoku;
 }
 
 
+
+
+
+//Function to put the data from the txt file in an array
 function sudokuInArray(sudoku){
     var sudoku_array=[
         [],
     ];
-
-    // sudoku_array[0][0]=1;
-    // sudoku_array[0][1]=4;
-    // sudoku_array[0][2]=2;
-    // sudoku_array[1][0]=1;
-    // sudoku_array[1][1]=8;
-    // sudoku_array[1][2]=7;
-
-
-    // console.log(sudoku_array);
     
     let row = 0;
     let col =0 ;
@@ -50,5 +55,66 @@ function sudokuInArray(sudoku){
             //console.log(sudoku_array);
         }
     }
-    console.log(sudoku_array);
+    return sudoku_array;
+    //console.log(sudoku_array);
+}
+
+
+
+//Function to resolve the sudoku
+function resolveSudoku(sudoku){
+
+    //Parcour the rows
+    for(let y=0; y<sudoku.length; y++){
+
+        //Parcour the cols
+        for(let x=0; x<(sudoku[y].length); x++){
+
+            if(sudoku[y][x] === "_"){
+                let new_value = 1;
+
+
+                while(new_value < 10){
+
+                    if(checkLine(sudoku[y], new_value)===true && checkCol(sudoku, x, new_value)===true){
+                        sudoku[y][x]=new_value;
+                        break;
+                    }
+
+
+
+                    new_value++;
+                }
+            }
+            
+        }
+    }
+return sudoku;
+}
+
+
+
+function checkLine(row, value){
+    //console.log(value);
+    let result = true;
+    for(let i=0; i<row.length; i++){
+        //console.log(i);
+        if(Number(row[i])===value){
+            result = false;
+        }
+    }
+    return result;
+}
+
+
+function checkCol(col, x, value){
+    //console.log(value);
+    let result = true;
+    for(let i=0; i<col.length; i++){
+        if(Number(col[i][x])===value){
+            result = false;
+            break;
+        }
+    }    
+    return result;
 }
