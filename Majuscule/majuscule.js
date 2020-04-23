b1b567a2e88d6863cc5bac52ca26229462869965
Luc Guimbertaud <luc.guimbertaud@orange.fr>
2020-04-23 22:30:18 +0200

    Update

--- Majuscule/majuscule.js
+++ Majuscule/majuscule.js
@@ -4,21 +4,20 @@ argument = String(argument).toLowerCase();
 var count = 1;
 
 for(var i=0; i<argument.length; i++){
-    //console.log(argument[i].indexOf(' '));
+    console.log(argument[i].indexOf(' '));
 
-    if(argument[i].indexOf(' ') == 0){   
+    if(argument[i].indexOf(' ') === 0){
     }
-    else if(count%2 == 0){
-        console.log(count);
+
+    if(count%2 == 0){
+        //console.log(count);
         argument = argument.replace(argument[i] ,argument[i].toUpperCase());
         count = count + 1;  
     }
     else{
-        
+        count = count + 1; 
     }
 }
-
-
 console.log(argument);
 
 
